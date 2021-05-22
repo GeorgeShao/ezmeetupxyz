df410cea0a2e28d65a51a9abf91db4656504c58a
George Shao <GeorgeShao123@gmail.com>
2021-05-22 13:17:53 -0400

    🔧 Setup Chakra UI components & theming

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react"
+import theme from './theme'
 
 ReactDOM.render(
   <React.StrictMode>
+    <ChakraProvider>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
