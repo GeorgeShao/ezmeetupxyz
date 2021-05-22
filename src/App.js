a269586d5503b440ccde78fb88e27742c187cdd4
George Shao <GeorgeShao123@gmail.com>
2021-05-22 13:41:00 -0400

    🔧 Setup React Router scaffolding

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import CreateRoom from "./routes/CreateRoom";
+import Room from "./routes/Room";
 
 function App() {
   return (
-    <div>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={CreateRoom} />
+        <Route path="/room/:roomID" component={Room} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
